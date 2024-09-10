a46dcf9fff3aa58191b6e886dec7d09fd92b0ccf
Abhishek <241abhishek@gmail.com>
2024-09-10 11:17:45 -0500

    Added an inference routine to process real robot input

--- nodes/action_predictor.py
+++ nodes/action_predictor.py
@@ -58,6 +58,7 @@ class ActionPredictor:
         self.checkpoint_path = rospy.get_param('checkpoint_path', '/home/cerebro/diff/src/diffusion_policy/data/epoch=0030-val_loss=0.018.ckpt')
 
         # Publishers
+        # these publishers are used with the simulation data
         self.patient_obs_pub = rospy.Publisher('patient_obs_topic', Float32MultiArray, queue_size=10)
         self.true_action_pub = rospy.Publisher('true_action_topic', Float32MultiArray, queue_size=10)
         self.predicted_action_pub = rospy.Publisher('predicted_action_topic', Float32MultiArray, queue_size=10)
@@ -101,18 +102,22 @@ class ActionPredictor:
         
         # Attributes for action prediction
         self.latency_counter = 0 # this counter is used to keep track of the latency of the action prediction
+        self.robot_state_counter = 0 # this counter is used to keep track of the robot state messages
         self.action_prediction_array = np.empty((0,4), dtype=np.float32) # this array is used to store the predicted actions
 
         # Subscribers
-        rospy.Subscriber('csv_topic', Float32MultiArray, self.true_obs_callback)
+        rospy.Subscriber('csv_topic', Float32MultiArray, self.csv_callback) # for simulation data
+        
+        # for the actual real robot
+        rospy.Subscriber('/X2_SRA_A/custom_robot_state', X2RobotState, self.custom_robot_state_callback)
 
         def shutdown_hook():
             rospy.loginfo("Shutting down action_predictor")
 
         rospy.on_shutdown(shutdown_hook)
 
-    def true_obs_callback(self, data):
-        """Callback for the true ground truth observation data"""
+    def csv_callback(self, data):
+        """Callback for the csv data used for simulating action prediction"""
         # increment the latency counter
         self.latency_counter += 1
 
@@ -167,6 +172,75 @@ class ActionPredictor:
         if self.enable_inference and not self.running_inference and self.patient_obs_queue.size() == 100:
             inference_thread = threading.Thread(target=self.run_inference)
             inference_thread.start()
+
+    def custom_robot_state_callback(self, data):
+        """Callback for the custom robot state with the real robot"""
+        # this callback only processes every 5 robot state messages
+        # which means that the effective rate of action prediction becomes 333/5 = 66.6 Hz
+        # predicted action messages are interpolated and published at 333 Hz
+        self.robot_state_counter += 1
+        # only run the inference process every 5 robot state messages
+        if self.robot_state_counter % 5 == 0:
+            self.robot_state_counter = 0 # reset the robot state counter
+            # increment the latency counter
+            self.latency_counter += 1
+
+            if self.action_prediction_array.size > 0:
+
+                # remove the first latency_counter rows from the action_prediction_array
+                # this is done to account for the delay in action prediction
+                self.action_prediction_array = self.action_prediction_array[self.latency_counter:]
+                # interpolate between the predicted actions to get the predicted actions at 333 Hz
+                scale_factor = 5 # number of interpolation points
+                x = np.arange(0, self.action_prediction_array.shape[0])
+                x_new = np.linspace(0, self.action_prediction_array.shape[0]-1, scale_factor*self.action_prediction_array.shape[0])
+                # perform interpolation using np.interp for each column of the action_prediction_array
+                interpolated_action_array = np.zeros((scale_factor*self.action_prediction_array.shape[0], 4), dtype=np.float32)
+                for i in range(4): # loop over the columns
+                    interpolated_action_array[:,i] = np.interp(x_new, x, self.action_prediction_array[:,i])
+                # convert the interpolated_action_array to a queue
+                self.predicted_action_queue.from_numpy(interpolated_action_array)
+                self.action_prediction_array = np.empty((0,4), dtype=np.float32) # reset the action_prediction_array
+
+                # reset the latency counter
+                self.latency_counter = 0
+
+            # parse the data
+            # first four elements are the patient observation
+            patient_obs = [data.joint_state[0], data.joint_state[1], data.joint_state[2], data.joint_state[3]]
+            # convert the patient observation to a numpy array
+            patient_obs_np = np.array(patient_obs)
+            # push the patient observation to the queue
+            self.patient_obs_queue.push(patient_obs_np)
+
+            # if the inference process is enabled, run an inference in a separate thread
+            if self.enable_inference and not self.running_inference and self.patient_obs_queue.size() == 100:
+                inference_thread = threading.Thread(target=self.run_inference)
+                inference_thread.start()
+
+        # interpolate and publish the predicted actions
+        if self.predicted_action_queue.size() > 0:
+            # fetch the predicted action from the predicted action queue
+            predicted_action = self.predicted_action_queue.queue.popleft()
+            assert predicted_action.size == 4, "Predicted action must have 4 elements"
+
+            # push the predicted action to the smoothed action window
+            self.smoothed_action_window.push(predicted_action)
+            # calculate the mean of the smoothed action window (which acts as the predicted action)
+            predicted_action = np.mean(self.smoothed_action_window.to_numpy(), axis=0)
+
+            # convert the predicted action to a numpy array of shape (1,4), dtype=np.float64
+            predicted_action = np.array(predicted_action, dtype=np.float64).reshape(1,4).squeeze().tolist()
+            # append a zero to the predicted action to match the size of the joint_state message
+            predicted_action.append(0.0) # this zero represents imu angle which is not used
+            # create a sensor_msgs.JointState message
+            joint_state_msg = sensor_msgs.msg.JointState()
+            joint_state_msg.header.stamp = rospy.Time.now()
+            joint_state_msg.position = predicted_action
+            joint_state_msg.velocity = [0.0, 0.0, 0.0, 0.0] # velocity is not used, set to zero for now
+
+            # publish the predicted action
+            self.predicted_action_pub.publish(joint_state_msg)
 
     def start_inference(self, req):
         """Starts the inference process"""
